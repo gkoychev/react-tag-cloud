ed2edd7edb229147cb2308a5984d3e965bd3c213
gkoychev <kgm.pochta@gmail.com>
2016-10-20 01:07:32 +0300

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var SRC = path.resolve(__dirname, "app");
+
+module.exports = {
+    entry: SRC + '/main.js',
+    output: { path: __dirname, filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                include: SRC,
+            }
+        ]
+    },
+};
